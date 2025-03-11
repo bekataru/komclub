08b9ea0ac1c4ad37a9d0b4fb67b20198567aac40
unknown <davitpapidze777@gmail.com>
2025-03-11 19:39:04 +0400

    added User model

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from backend.backend.model_utils.models import TimeStampedModel
 
-# Create your models here.
+class User(AbstractUser, TimeStampedModel):
+    email = models.EmailField(unique=True)
+
+    REQUIRED_FIELDS = ['email']
