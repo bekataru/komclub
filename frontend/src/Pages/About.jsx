2fb923692969ae47f588d2d3a1bc8f096c085100
ბექა <91470466+bekatar@users.noreply.github.com>
2025-03-11 20:41:51 +0400

    added routes

---
+++ frontend/src/Pages/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className="about-page">
+      <h1>About Page</h1>
+      <p>This is the about page of my application. Here you can learn more about what we do.</p>
+    </div>
+  );
+};
+
+export default About;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
+import Home from './Pages/Home.jsx'
+import About from './Pages/About.jsx'
+import NotFound from './Pages/NotFound.jsx'
+import Layout from './components/Layout.jsx'
+import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
 )
