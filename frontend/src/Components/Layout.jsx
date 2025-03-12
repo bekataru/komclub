f3a31feb5d3aa3036c674d88960647428469a5e5
ბექა <91470466+bekatar@users.noreply.github.com>
2025-03-12 23:26:26 +0400

    added basic footer/navigation, login page.

--- frontend/src/Components/Layout.jsx
+++ frontend/src/Components/Layout.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Link, Outlet } from 'react-router-dom';
-
+import '../Style/Layout.css';
+import Logo from '../assets/images/logo.png';
 const Layout = () => {
   return (
     <div className="layout">
       <header>
-        <nav>
-          <ul style={{ display: 'flex', listStyle: 'none', gap: '1rem' }}>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About</Link></li>
+        <nav className="navigation">
+          <ul className="navigation-bar">
+            <img className="komclub-logo" src={Logo} alt="KomClub"></img>
+            <li className="navigation-bar-item"><Link to="/">Home</Link></li>
+            <li className="navigation-bar-item"><Link to="/about">About</Link></li>
           </ul>
         </nav>
       </header>
